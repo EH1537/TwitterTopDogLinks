2736cbeff81fa25f5c385591671f7b3419218bbf
EH1537 <ehilton1537@gmail.com>
2020-05-17 16:38:12 -0800

    testing server changes

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ const PORT = secretsAndKeys.PORT
 
 console.log("node env file", process.env.TESTER)
 
+console.log(secretsAndKeys.PROXY_PORT)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
